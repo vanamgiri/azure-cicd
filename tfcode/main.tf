3077f9c6015b042b5bd709672c26dae5316dc1d9
Giri Vanam <39965642+vanamgiri@users.noreply.github.com>
2022-01-13 14:57:03 -0500

    Update main.tf

--- tfcode/main.tf
+++ tfcode/main.tf
@@ -8,6 +8,6 @@ provider "azurerm" {
   features {}
 }
 resource "azurerm_resource_group" "rg" {
-  name     = "resourcegroup-test-tbd"
+  name     = "resourcegroup-test-tbd-1"
   location = "eastus2"
 }
