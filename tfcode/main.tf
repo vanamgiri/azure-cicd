f53fdbccb275ea6e2d97b4edd3baff590fcd44da
Giri Vanam <39965642+vanamgiri@users.noreply.github.com>
2022-01-13 12:51:56 -0500

    Update main.tf

--- tfcode/main.tf
+++ tfcode/main.tf
@@ -4,12 +4,12 @@ terraform {
 }
 
 provider "azurerm" {
-  version = ">=2.0"
+  version = ">=2.91.0"
   # The "feature" block is required for AzureRM provider 2.x.
   features {}
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "resourcegroup-test-tbd-3"
-  location = "westeurope"
+  name     = "resourcegroup-test-tbd"
+  location = "eastus2"
 }
