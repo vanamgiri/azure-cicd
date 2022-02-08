f7788e6e505a7a7d375734ca14cd1a3297b28de2
vanamgiri <girivanam6@outlook.com>
2022-02-07 19:55:08 -0500

    removed ouputs groups

--- tfcode/outputs.tf
+++ tfcode/outputs.tf
@@ -1,13 +1,13 @@
 
-# output "vnet_id" {
-#   description = "The id of the newly created vNet"
-#   value       = azurerm_virtual_network.vnet.id
-# }
+ output "vnet_id" {
+   description = "The id of the newly created vNet"
+   value       = azurerm_virtual_network.app_network.id
+ }
 
-# output "vnet_name" {
-#   description = "The name of the newly created vNet"
-#   value       = azurerm_virtual_network.vnet.name
-# }
+#  output "vnet_name" {
+#    description = "The name of the newly created vNet"
+#    value       = azurerm_virtual_network.vnet.name
+#  }
 
 # output "vnet_location" {
 #   description = "The location of the newly created vNet"
